e014008c1be23c56cc5934eef858b52013286b8b
Morten Minde Neergaard <mneergaa@cisco.com>
2013-08-15 01:58:37 +0200

    I made my own timing functions. Again, ooops...

---
+++ time.h
@@ -0,0 +1,66 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+typedef uint32_t time_t;
+
+enum week_day {
+    Monday = 0, Tuesday = 1, Wednesday = 2, Thursday = 3, Friday = 4, Saturday = 5, Sunday = 6
+};
+
+struct time_data {
+  uint8_t second;
+  uint8_t minute;
+  uint8_t hour;
+  uint8_t week_day;
+  //uint8_t day;
+  //uint8_t month;
+  //uint8_t year;
+};
+
+typedef void (*request_time_function_t)(void);
+
+// constants
+#define SECS_PER_MIN 60UL
+#define MINS_PER_HOUR 60UL
+#define SECS_PER_HOUR 3600UL
+#define SECS_PER_DAY 86400UL
+#define HOURS_PER_DAY 24UL
+#define DAYS_PER_WEEK 7UL
+#define SECS_PER_WEEK 604800UL
+
+// Conversion macros
+#define get_second(time) (time % SECS_PER_MIN)
+#define get_minute(time) ((time / MINS_PER_HOUR) % MINS_PER_HOUR)
+#define get_hour(time) ((time % SECS_PER_DAY) / SECS_PER_HOUR)
+#define get_week_day(time) (((time / SECS_PER_DAY + Thursday) % DAYS_PER_WEEK))
+#define minutes_in_seconds(minutes) (minutes * SECS_PER_MIN)
+#define hours_in_seconds(hours) (hours * SECS_PER_HOUR)
+#define days_in_seconds(days) (days * SECS_PER_DAY)
+
+// Calculation macros
+#define elapsed_seconds_today(time) (time % SECS_PER_DAY)
+// Which is better?
+#define last_midnight(time) ((time / SECS_PER_DAY) * SECS_PER_DAY)
+//#define last_midnight(time) (time - elapsed_seconds_today(time))
+// And here, too... ?
+//#define next_midnight(time) (last_midnight(time) + SECS_PER_DAY)
+#define next_midnight(time) ((time / SECS_PER_DAY + 1) * SECS_PER_DAY)
+//#define next_midnight(time) (time + (SECS_PER_DAY - elapsed_seconds_today(time)))
+
+inline time_t get_current_timestamp(void);
+inline bool time_is_set(void);
+inline time_t get_measured_time_drift(void);
+
+inline time_t get_time(void);
+inline void update_time(void);
+inline void set_time(time_t t);
+
+inline void set_time_requester(request_time_function_t);
+inline void set_sync_interval(time_t);
+
+inline void split_timestamp(time_t, struct time_data *);
+
+#endif
---
+++ time.ino
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+
+#include "time.h"
+
+// "Globally" available time data through getters
+static time_t current_timestamp = 0;
+static bool time_has_been_set = false;
+static time_t measured_time_drift = 0;
+
+static uint32_t sync_interval = 300;
+static uint32_t previous_millis = 0;
+static uint32_t next_sync_threshold = 0;
+
+static request_time_function_t time_requester = NULL;
+
+inline void split_timestamp(time_t time, struct time_data * data){
+    data->second = time % SECS_PER_MIN;
+    time /= SECS_PER_MIN;
+    data->minute = time % MINS_PER_HOUR;
+    time /= MINS_PER_HOUR;
+    data->hour = time % HOURS_PER_DAY;
+    time /= HOURS_PER_DAY;
+    data->week_day = ((time + Thursday) % DAYS_PER_WEEK);
+}
+
+inline void update_time(void) {
+    time_t current_millis = millis();
+    current_timestamp += (current_millis - previous_millis) / 1000UL;
+    previous_millis = current_millis;
+
+    if (next_sync_threshold <= current_timestamp && time_requester != NULL)
+        (*time_requester)(), next_sync_threshold = current_timestamp + sync_interval;
+}
+
+inline time_t get_time(void) {
+    return current_timestamp;
+}
+
+inline void set_time(time_t t) {
+    update_time();
+    next_sync_threshold = t + sync_interval;
+    if (time_has_been_set) {
+        measured_time_drift += current_timestamp - t;
+    } else {
+        time_has_been_set = true;
+        previous_millis = millis();
+    }
+    current_timestamp = t;
+}
+
+inline void set_time_requester(request_time_function_t requester) {
+    time_requester = requester;
+    (*requester)();
+}
+
+inline void set_sync_interval(time_t interval) {
+    next_sync_threshold += interval - sync_interval;
+    sync_interval = interval;
+}
+
+inline time_t get_current_timestamp(void) {
+    return current_timestamp;
+}
+inline bool time_is_set(void) {
+    return time_has_been_set;
+}
+inline time_t get_measured_time_drift(void) {
+    return measured_time_drift;
+}
