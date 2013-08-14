06fa301889537affcc4f681a8afcfce03671d38d
Morten Minde Neergaard <mneergaa@cisco.com>
2013-08-15 01:58:04 +0200

    I made my own pcd8544 driver. Ooops.

---
+++ pcd8544.h
@@ -0,0 +1,72 @@
+#ifndef PCD8544_H
+#define PCD8544_H
+
+#include <inttypes.h>
+#include <stdbool.h>
+
+// The display spec
+#define PCD8544_WIDTH 84
+#define PCD8544_HEIGHT 48
+#define PCD8544_LINES 6
+#define PCD8544_COLS 14
+
+// Default pin assignments. Note thet DIN and CLK are set by hardware SPI.
+#ifndef PCD8544_PIN_DC
+#define PCD8544_PIN_DC 0
+#endif
+#ifndef PCD8544_PIN_RST
+#define PCD8544_PIN_RST 0
+#endif
+#ifndef PCD8544_PIN_SSEL
+#define PCD8544_PIN_SSEL 0
+#endif
+#ifndef PCD8544_PIN_BL
+#define PCD8544_PIN_BL 0
+#endif
+
+// Default contrast value
+#ifndef PCD8544_CONTRAST
+#define PCD8544_CONTRAST 0x3F
+#endif
+
+// LCD commands, Table 1, page 14
+#define PCD8544_FUNCTION_SET (1<<5)
+#define PCD8544_FUNCTION_PD  (1<<2)
+#define PCD8544_FUNCTION_V   (1<<1)
+#define PCD8544_FUNCTION_H   (1<<0)
+
+// Normal instructions, H = 0
+#define PCD8544_DISPLAY_CONTROL (1<<3)
+#define PCD8544_DISPLAY_CONTROL_D (1<<2)
+#define PCD8544_DISPLAY_CONTROL_E (1<<0)
+#define PCD8544_DISPLAY_CONTROL_BLANK 0
+#define PCD8544_DISPLAY_CONTROL_NORMAL_MODE  PCD8544_DISPLAY_CONTROL_D
+#define PCD8544_DISPLAY_CONTROL_ALL_ON       PCD8544_DISPLAY_CONTROL_E
+#define PCD8544_DISPLAY_CONTROL_INVERSE      (PCD8544_DISPLAY_CONTROL_D|PCD8544_DISPLAY_CONTROL_E)
+
+#define PCD8544_SET_LINE_ADDRESS (1<<6)
+#define PCD8544_LINE_ADRESS_MASK 0b111
+#define PCD8544_SET_X_ADDRESS (1<<7)
+#define PCD8544_X_ADRESS_MASK 0b01111111
+
+// Extended instructions. H = 1
+#define PCD8544_TEMP_CONTROL (1<<2)
+#define PCD8544_TEMP_TC1     (1<<1)
+#define PCD8544_TEMP_TC0     (1<<0)
+
+#define PCD8544_BIAS     (1<<4)
+#define PCD8544_BIAS_BS2 (1<<2)
+#define PCD8544_BIAS_BS1 (1<<1)
+#define PCD8544_BIAS_BS0 (1<<0)
+
+#define PCD8544_VOP (1<<7)
+#define PCD8544_SET_CONTRAST PCD8544_VOP
+
+inline void pcd8544_init(void);
+inline void pcd8544_clear(void);
+inline void pcd8544_place_cursor(uint8_t column, uint8_t row);
+inline void pcd8544_write_char(char);
+inline void pcd8544_write_string(char *);
+inline void pcd8544_newline();
+inline void pcd8544_set_backlight_state(bool);
+#endif
---
+++ pcd8544.ino
@@ -0,0 +1,97 @@
+#include <stdbool.h>
+
+#include <SPI.h>
+
+#include "ascii.h"
+#include "pcd8544.h"
+
+#define set_is_data() digitalWrite(PCD8544_PIN_DC, 1)
+#define set_is_command() digitalWrite(PCD8544_PIN_DC, 0)
+
+uint8_t current_col = 0;
+uint8_t current_line = 0;
+
+inline void inc_col(void) {
+    if (++current_col >= PCD8544_COLS) {
+        current_col = 0;
+        if (++current_line >= PCD8544_LINES)
+            current_line = 0;
+    }
+}
+
+inline void pcd8544_init(void) {
+    pinMode(PCD8544_PIN_BL, OUTPUT);
+    pinMode(PCD8544_PIN_DC, OUTPUT);
+    pinMode(PCD8544_PIN_RST, OUTPUT);
+    pinMode(PCD8544_PIN_SSEL, OUTPUT);
+    SPI.begin();
+
+    digitalWrite(PCD8544_PIN_RST, 0);
+    delay(1);
+    digitalWrite(PCD8544_PIN_RST, 1);
+    digitalWrite(PCD8544_PIN_SSEL, 0);
+
+    set_is_command();
+    SPI.transfer(PCD8544_FUNCTION_SET | PCD8544_FUNCTION_H);
+    SPI.transfer(PCD8544_SET_CONTRAST | PCD8544_CONTRAST);
+    SPI.transfer(PCD8544_TEMP_CONTROL);
+    SPI.transfer(PCD8544_BIAS | PCD8544_BIAS_BS1 | PCD8544_BIAS_BS0);
+    SPI.transfer(PCD8544_FUNCTION_SET);
+    SPI.transfer(PCD8544_DISPLAY_CONTROL | PCD8544_DISPLAY_CONTROL_NORMAL_MODE);
+    set_is_data();
+}
+
+inline void pcd8544_place_cursor(uint8_t column, uint8_t line) {
+    if (line >= PCD8544_LINES)
+        line %= PCD8544_LINES;
+    if (column >= PCD8544_COLS)
+        column %= PCD8544_COLS;
+    // Yes, x is pixel addressed, y is line addressed This makes sense because
+    // we write 8 pixels at a time.
+    current_col = column;
+    current_line = line;
+    set_is_command();
+    SPI.transfer(PCD8544_SET_X_ADDRESS | (current_col * 6));
+    SPI.transfer(PCD8544_SET_LINE_ADDRESS | current_line);
+    set_is_data();
+}
+
+inline void pcd8544_write_char(char value) {
+    if (value == '\n' || value == '\0') {
+        // TODO this may not handle all cases, e.g. when a '\0' is the 15th
+        // char of a line...
+        pcd8544_newline();
+        return;
+    }
+    const uint8_t * data = ASCII[value - ' '];
+    for (int i = 0 ; i != 5 ; i++)
+        SPI.transfer(data[i]);
+    SPI.transfer(0x00); // 8 blank bits to separate chars
+    inc_col();
+}
+
+inline void pcd8544_write_string(char * data) {
+    while (*data == '\n')
+        pcd8544_newline(), data++;
+
+    do
+        pcd8544_write_char(*data);
+    while (*data++ != '\0');
+}
+
+inline void pcd8544_clear(void) {
+    for (uint8_t i = 0; i != (PCD8544_WIDTH * PCD8544_LINES); i++)
+        SPI.transfer(0x00);
+}
+
+inline void pcd8544_newline(void) {
+    while (current_col != 0) {
+        for (uint8_t i = 0; i != 6; i++)
+            SPI.transfer(0x00);
+        inc_col();
+    }
+}
+
+inline void pcd8544_set_backlight_state(bool value) {
+    digitalWrite(PCD8544_PIN_BL, value);
+}
