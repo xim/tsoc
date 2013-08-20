25a1a23c6496616792dd78ced27a985a0086757a
Morten Minde Neergaard <mneergaa@cisco.com>
2013-08-20 10:38:25 +0200

    Make stuff compile by removing unfinished work

--- cwappy/libcwapmodule.c
+++ cwappy/libcwapmodule.c
@@ -1,24 +1,5 @@
 #include <Python.h>
 #include "../libcwap.h"
-// -------------------------------------
-// WIP: pseudocode
-
-static PyObject * fun = NULL;
-
-static size_t read_wrapper(char * buf, size_t size) {
-    PyObject * retval = fun(buf, size);
-    PyString * str = (PyString *) retval;
-    size_t ret_size = PySize(retval);
-    memcpy(from_python, buf, str->size);
-    return str->size
-}
-
-static void action(PyObject * args) {
-    PyObject * read_function = magic(args);
-    fun = read_function;
-    libcwap_action(read_wrapper);
-}
-// ----------------------------------
 
 static PyObject * serial_reader = NULL;
 
@@ -64,8 +45,8 @@ static PyMethodDef LibCwapMethods[] = {
 
     {"action",  pycwap_action, METH_VARARGS,
      "Make things happen."},
-    {"register",  pycwap_register, METH_VARARGS | METH_KEYWORDS,
-     "Register new things."},
+    //{"register",  pycwap_register, METH_VARARGS | METH_KEYWORDS,
+    // "Register new things."},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
