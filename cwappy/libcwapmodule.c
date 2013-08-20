b6ee429af43d4ed3a5912de3c64bc389d26c473e
Morten Minde Neergaard <mneergaa@cisco.com>
2013-08-20 10:49:02 +0200

    What appears to be a working python C binding thing

--- cwappy/libcwapmodule.c
+++ cwappy/libcwapmodule.c
@@ -7,37 +7,37 @@ static size_t action_read_wrapper(char * buf, size_t size) {
     //these args are set up by libcwap.c, just fill'em with pythonic goodness
     PyObject *result = NULL;
     PyObject *arglist;
-    int ret_size = 0;
+    Py_ssize_t ret_size = 0;
     // Create a tuple to feed back to python
-    arglist = Py_BuildValue("(s,)", size);
+    arglist = Py_BuildValue("I", size);
     // Actually call python: Have python grab size bytes from GPIO, I think?
     result = PyObject_CallObject(serial_reader, arglist);
     Py_DECREF(arglist);
     if (result == NULL)
         return 0; // stuff went tits-up.
-    if (PyArg_ParseTuple(result, "s", buf))
-        ret_size = size; // TODO Should verify length of s
+    char from_python[size];
+    if (!PyArg_ParseTuple(result, "s#", from_python, &ret_size))
+        ret_size = 0;
+    memcpy(buf, from_python, ret_size);
     Py_DECREF(result);
     return ret_size;
 }
 
 static PyObject * pycwap_action(PyObject *dummy, PyObject *args) {
     PyObject *result = NULL;
-    PyObject *temp;
 
-    if (PyArg_ParseTuple(args, "O:set_callback", &temp)) {
-        if (!PyCallable_Check(temp)) {
-            PyErr_SetString(PyExc_TypeError, "parameter must be callable");
+    if (PyArg_ParseTuple(args, "O:set_callback", &serial_reader)) {
+        if (!PyCallable_Check(serial_reader)) {
+            PyErr_SetString(PyExc_TypeError, "Parameter must be callable");
             return NULL;
         }
-        Py_XINCREF(temp);         /* Add a reference to new callback */
-        Py_XDECREF(serial_reader);  /* Dispose of previous callback */
-        serial_reader = temp;       /* Remember new callback */
+        Py_XINCREF(serial_reader); /* Increment reference count to new callback */
         /* Boilerplate to return "None" */
         Py_INCREF(Py_None);
         result = Py_None;
     }
     libcwap_action(action_read_wrapper);
+    Py_XDECREF(serial_reader);  /* Dispose of callback */
     return result;
 }
 
