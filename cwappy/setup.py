4272da09ed533325f9715fb92fa4b79e1969db89
Oyvind Tonsberg <otonsber@cisco.com>
2013-08-20 19:32:58 +0200

    added extra compile args to setup.py

--- cwappy/setup.py
+++ cwappy/setup.py
@@ -3,4 +3,7 @@
 from distutils.core import setup, Extension
 
 setup(name='libcwap', version='0.1',
-        ext_modules=[Extension('libcwap', ['libcwapmodule.c', '../libcwap.c'])])
+        ext_modules=[Extension(
+            'libcwap',
+            ['libcwapmodule.c', '../libcwap.c'],
+            extra_compile_args=['-std=c99', '-Werror'])])
