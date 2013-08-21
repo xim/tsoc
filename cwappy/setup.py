cdd3f299a7443bec3842b77b47a3799685ae1c3e
Morten Minde Neergaard <mneergaa@cisco.com>
2013-08-21 12:06:24 +0200

    Rebuild if libcwap.h changes

--- cwappy/setup.py
+++ cwappy/setup.py
@@ -6,4 +6,5 @@ setup(name='libcwap', version='0.1',
         ext_modules=[Extension(
             'libcwap',
             ['libcwapmodule.c', '../libcwap.c'],
+            depends=['../libcwap.h'],
             extra_compile_args=['-std=c99', '-Wall', '-Wextra', '-Werror'])])
